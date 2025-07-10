0b60928d00c7f17db0c3e7256f1ae0830283b54e
achu <anchovy999@gmail.com>
2025-07-10 23:26:01 +0900

    tailwindcss設定など

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '')
+  // const env = loadEnv(mode, '.', '')
   return {
     resolve: {
       alias: {
